30c657b98552d762e6bdcb09ed1f61556fda28fd
Nicckx <71626472+NickAzarafroz@users.noreply.github.com>
2023-06-21 17:07:48 +0200

    - Removed Subject.cpp # Modified Subject.h -> Due to linking errors caused by template the  definitions are moved to the header file

--- Minigin/Subject.h
+++ Minigin/Subject.h
@@ -9,16 +9,34 @@ namespace dae
 	{
 	public:
 		Subject() = default;
-		~Subject();
+		~Subject() 
+		{
+			for (auto& observer : m_Observers)
+			{
+				observer->OnSubjectDestroy();
+			}
+		}
 
 		Subject(const Subject& other) = delete;
 		Subject(Subject&& other) = delete;
 		Subject& operator=(const Subject& other) = delete;
 		Subject& operator=(Subject&& other) = delete;
 
-		void AddObserver(Observer<Args...>* pObserver);
-		void RemoveObserver(Observer<Args...>* pObserver);
-		void Notify(Args... args);
+		void AddObserver(Observer<Args...>* pObserver)
+		{
+			m_Observers.emplace_back(pObserver);
+		}
+		void RemoveObserver(Observer<Args...>* pObserver)
+		{
+			m_Observers.erase(std::remove(m_Observers.begin(), m_Observers.end(), pObserver), m_Observers.end());
+		}
+		void Notify(Args... args)
+		{
+			for (auto& observer : m_Observers)
+			{
+				observer->HandleEvent(args...);
+			}
+		}
 
 	private:
 		std::vector<Observer<Args...>*> m_Observers;
